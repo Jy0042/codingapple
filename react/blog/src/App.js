807daad79aa5377362fba65ac2196d2b0f27318e
jy0042 <sjy6630@gmail.com>
2024-05-04 17:21:10 +0900

    study: react array, object state 변경하는 법

--- react/blog/src/App.js
+++ react/blog/src/App.js
@@ -4,9 +4,9 @@ import "./App.css";
 function App() {
   let [logo, setLogo] = useState("React Blog");
   let [title, setTitle] = useState([
-    "여자 옷 추천",
     "남자 옷 추천",
-    "맛집 추천",
+    "리액트 공부",
+    "강남 맛집 추천",
   ]);
   let [like, setLike] = useState(0);
 
@@ -15,6 +15,31 @@ function App() {
       <div className="black-nav">
         <h4 style={{ color: "yellow", fontSize: "16px" }}>{logo}</h4>
       </div>
+
+      <button
+        onClick={() => {
+          let titleChange = [...title];
+          if (titleChange[0] === "남자 옷 추천") {
+            titleChange[0] = "여자 옷 추천";
+          } else {
+            titleChange[0] = "남자 옷 추천";
+          }
+          setTitle(titleChange);
+        }}
+      >
+        Change Title
+      </button>
+
+      <button
+        onClick={() => {
+          let sortTitle = [...title];
+          sortTitle.sort();
+          setTitle(sortTitle);
+        }}
+      >
+        이름순 정렬
+      </button>
+
       <div className="list">
         <h4>
           {title[0]}{" "}
