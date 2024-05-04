eadc5d608840fb8f6322bfa7e6f25e63fd427921
jy0042 <sjy6630@gmail.com>
2024-05-04 16:21:57 +0900

    study: react state 사용법과 변수 문법과의 차이

--- react/blog/src/App.js
+++ react/blog/src/App.js
@@ -1,15 +1,32 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  let post = "강남 우동 맛집";
+  let [logo, setLogo] = useState("React Blog");
+  let [title, setTitle] = useState([
+    "여자 옷 추천",
+    "남자 옷 추천",
+    "맛집 추천",
+  ]);
 
   return (
-    <div className="App">
+    <>
       <div className="black-nav">
-        <h4 style={{ color: "yellow", fontSize: "16px" }}>Blog</h4>
+        <h4 style={{ color: "yellow", fontSize: "16px" }}>{logo}</h4>
       </div>
-      <h4>{post}</h4>
-    </div>
+      <div className="list">
+        <h4>{title[0]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{title[1]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{title[2]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+    </>
   );
 }
 
