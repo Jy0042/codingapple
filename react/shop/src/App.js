67f12d8e5e56f5b98731c92dbf92ef899b261e40
jy0042 <sjy6630@gmail.com>
2024-05-06 00:54:02 +0900

    study: react component props와 map 두가지 방식으로 가져와보기

---
+++ react/shop/src/App.js
@@ -0,0 +1,55 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import bg from "./img/bg.png";
+import Products from "./components/Products";
+import { useState } from "react";
+import data from "./data2.js";
+
+function App() {
+  const [shoes] = useState(data);
+
+  return (
+    <div className="App">
+      <Navbar bg="primary" data-bs-theme="primary" className="custom-navbar">
+        <Container>
+          <Navbar.Brand href="#home">React Shop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Community</Nav.Link>
+            <Nav.Link href="#pricing">Cart</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <div
+        className="main-bg"
+        style={{ backgroundImage: "url(" + bg + ")" }}
+      ></div>
+      <div className="container">
+        <div className="row">
+          <Card shoes={shoes[0]} i={1} />
+          <Card shoes={shoes[1]} i={2} />
+          <Card shoes={shoes[2]} i={3} />
+          <Products />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Card(props) {
+  return (
+    <div className="col-md-4">
+      <img
+        src={"https://codingapple1.github.io/shop/shoes" + props.i + ".jpg"}
+        width="80%"
+      />
+      <h4>{props.shoes.title}</h4>
+      <p>{props.shoes.content}</p>
+      <p>{props.shoes.price}</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ react/shop/src/components/Products.jsx
@@ -0,0 +1,22 @@
+import data from "../data.js";
+import { useState } from "react";
+
+export default function Products() {
+  const [shoes, setShoes] = useState(data);
+
+  return (
+    <>
+      {shoes.map((item, i) => {
+        return (
+          <div className="col-md-4" key={i}>
+            {/* <img src={process.env.PUBLIC_URL + "/on.webp"} width="80%" /> */}
+            <img src={process.env.PUBLIC_URL + item.image} width="80%" />
+            <h4>{item.title}</h4>
+            <p>{item.content}</p>
+            <p>{item.price}</p>
+          </div>
+        );
+      })}
+    </>
+  );
+}
