c040a462b687df91b5ca1f0a825e97706af019e0
jy0042 <sjy6630@gmail.com>
2024-05-05 19:15:31 +0900

    study: react class 문법

--- react/blog/src/App.js
+++ react/blog/src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import "./App.css";
 import Header from "./components/Header";
 import List from "./components/List";
@@ -7,8 +8,34 @@ function App() {
     <>
       <Header />
       <List />
+      <Modal2 name="철수" age={30} />
     </>
   );
+}
+
+class Modal2 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "kim",
+      age: 20,
+    };
+  }
+  render() {
+    return (
+      <div>
+        안녕 {this.props.name} 나이는 {this.state.age}
+        나는 {this.props.age}
+        <button
+          onClick={() => {
+            this.setState({ age: 23 });
+          }}
+        >
+          버튼
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
