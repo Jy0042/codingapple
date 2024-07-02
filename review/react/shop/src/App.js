60c5a6d8fbba2d62d4dd887f4c2441057f2b27c8
jy0042 <sjy6630@gmail.com>
2024-07-02 14:39:46 +0900

    study: 반복 되는 요소 컴포넌트화 시키기

--- review/react/shop/src/App.js
+++ review/react/shop/src/App.js
@@ -1,7 +1,12 @@
+import { useState } from "react";
 import "./App.css";
-import { Button, Navbar, Container, Nav } from "react-bootstrap";
+import { Navbar, Container, Nav, Row, Col } from "react-bootstrap";
+import bg from "./img/bg.png";
+import data from "./data.js";
 
 function App() {
+  let [shoes] = useState(data);
+
   return (
     <div className="App">
       <Navbar bg="primary" data-bs-theme="dark">
@@ -9,13 +14,31 @@ function App() {
           <Navbar.Brand href="#home">Shop</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#features">Cart</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
+
+      <div className="main-bg" style={{ backgroundImage: "url(" + bg + ")" }}></div>
+
+      <Row>
+        {shoes.map((s, i) => (
+          <Product key={i} shoes={s} />
+        ))}
+      </Row>
     </div>
   );
 }
 
 export default App;
+
+function Product(props) {
+  return (
+    <Col>
+      <img src={`https://codingapple1.github.io/shop/shoes${props.shoes.id + 1}.jpg`} width="80%" />
+      <h4>{props.shoes.title}</h4>
+      <p>{props.shoes.content}</p>
+      <p>{props.shoes.price}</p>
+    </Col>
+  );
+}
