06effe445393948828ee76a72276437502a86e3c
jy0042 <sjy6630@gmail.com>
2024-07-02 15:27:35 +0900

    study: react router 사용법

--- review/react/shop/src/App.js
+++ review/react/shop/src/App.js
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import "./App.css";
 import { Navbar, Container, Nav, Row, Col } from "react-bootstrap";
-import bg from "./img/bg.png";
+// import bg from "./img/bg.png";
 import data from "./data.js";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   let [shoes] = useState(data);
@@ -19,13 +20,25 @@ function App() {
         </Container>
       </Navbar>
 
-      <div className="main-bg" style={{ backgroundImage: "url(" + bg + ")" }}></div>
+      <Link to="/">홈</Link>
+      <Link to="/detail">상세페이지</Link>
 
-      <Row>
-        {shoes.map((s, i) => (
-          <Product key={i} shoes={s} />
-        ))}
-      </Row>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <div className="main-bg"></div>
+              <Row>
+                {shoes.map((s, i) => (
+                  <Product key={i} shoes={s} />
+                ))}
+              </Row>
+            </>
+          }
+        />
+        <Route path="/detail" element={<DetailPage />} />
+      </Routes>
     </div>
   );
 }
@@ -34,11 +47,29 @@ export default App;
 
 function Product(props) {
   return (
-    <Col>
+    <Col sm>
       <img src={`https://codingapple1.github.io/shop/shoes${props.shoes.id + 1}.jpg`} width="80%" />
       <h4>{props.shoes.title}</h4>
       <p>{props.shoes.content}</p>
       <p>{props.shoes.price}</p>
     </Col>
+  );
+}
+
+function DetailPage() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6">
+          <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+        </div>
+        <div className="col-md-6">
+          <h4 className="pt-5">상품명</h4>
+          <p>상품설명</p>
+          <p>120000원</p>
+          <button className="btn btn-danger">주문하기</button>
+        </div>
+      </div>
+    </div>
   );
 }
