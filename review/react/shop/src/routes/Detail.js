7bb5e8e42a09e79dc33cfc98b5d3f50549c403e1
jy0042 <sjy6630@gmail.com>
2024-07-03 10:42:46 +0900

    study: styled-components 사용법

--- review/react/shop/src/routes/Detail.js
+++ review/react/shop/src/routes/Detail.js
@@ -1,14 +1,32 @@
 import { useParams } from "react-router-dom";
+import styled from "styled-components";
+
+// const YellowButton = styled.button`
+//   background: ${(props) => props.color};
+//   color: ${(props) => (props.color === "skyblue" ? "white" : "black")};
+//   padding: 10px;
+//   border-radius: 5px;
+//   cursor: pointer;
+// `;
+
+// const Box = styled.div`
+//   background: gray;
+//   padding: 20px;
+// `;
 
 export default function DetailPage(props) {
   let { id } = useParams();
   let imgId = parseInt(id) + 1;
   let product = props.shoes.find(function (x) {
-    return x.id == id;
+    return x.id === parseInt(id);
   });
 
   return (
     <div className="container">
+      {/* <Box>
+        <YellowButton color="skyblue">버튼</YellowButton>
+        <YellowButton color="lightpink">버튼</YellowButton>
+      </Box> */}
       <div className="row">
         <div className="col-md-6">
           <img src={`https://codingapple1.github.io/shop/shoes${imgId}.jpg`} width="100%" />
